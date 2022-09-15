1c9ca0e4cdde5302c05582b1968eb2e807de6da2
Kolisnychenko-Alona <tisspac@gmail.com>
2022-09-15 17:25:54 +0300

    add some changes

--- src/app/lesson3/lesson3.component.ts
+++ src/app/lesson3/lesson3.component.ts
@@ -3,57 +3,82 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-lesson3',
   templateUrl: './lesson3.component.html',
-  styleUrls: ['./lesson3.component.scss']
+  styleUrls: ['./lesson3.component.scss'],
 })
 export class Lesson3Component implements OnInit {
   public name!: string;
-  public isDone = false;
   public isEdit = false;
   public editName = '';
   public indexEdit!: number;
-  public tasks: Array<any> = [];
+  public tasks: Array<any> = [
+    {
+      name: 'Java Script',
+      checked: true,
+      disabled: false,
+      delBtnClass: 'deleteBtn',
+    },
+    {
+      name: 'jQuery',
+      checked: true,
+      disabled: false,
+      delBtnClass: 'deleteBtn',
+    },
+    {
+      name: 'Angular',
+      checked: false,
+      disabled: true,
+      delBtnClass: 'deleteBtn disabled',
+    },
+    {
+      name: 'Bootstrap',
+      checked: false,
+      disabled: true,
+      delBtnClass: 'deleteBtn disabled',
+    },
+    {
+      name: 'HTML5',
+      checked: false,
+      disabled: true,
+      delBtnClass: 'deleteBtn disabled',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   addTask(): void {
-    if(this.name){
+    if (this.name) {
       let task = {
         name: this.name,
-        done: this.isDone,
         checked: false,
         disabled: true,
-        delBtnClass: 'deleteBtn disabled'
-      }
-      this.tasks.push(task)
+        delBtnClass: 'deleteBtn disabled',
+      };
+      this.tasks.push(task);
       this.name = '';
     }
-    
   }
   getStatus(indexCheckBox: number): void {
     if (!this.tasks[indexCheckBox].checked) {
       this.tasks[indexCheckBox].disabled = true;
       this.tasks[indexCheckBox].delBtnClass += ' disabled';
-    }
-    else {
+    } else {
       this.tasks[indexCheckBox].disabled = false;
       this.tasks[indexCheckBox].delBtnClass = 'deleteBtn';
     }
   }
-  editTask(index: number): void{
+  editTask(index: number): void {
     this.indexEdit = index;
     this.isEdit = true;
     this.editName = this.tasks[this.indexEdit].name;
   }
-  saveName(): void{
+  saveName(): void {
     this.tasks[this.indexEdit].name = this.editName;
     this.editName = '';
     this.isEdit = false;
   }
-  deleteTask(index: number): void{
-    this.tasks.splice(index, 1)
+  deleteTask(index: number): void {
+    this.tasks.splice(index, 1);
   }
-
 }
